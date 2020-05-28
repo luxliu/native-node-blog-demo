20d1e22fd517ad838ae5d76efd8460676f24e75c
Lux <luxliu007168@gmail.com>
2020-05-28 16:03:59 +1200

    use cookie and local session for auth

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const queryString = require('querystring');
 const handleBlogRouter = require('./src/router/blog');
 const handleUserRouter = require('./src/router/user');
 
+const SESSION_DATA = {};
+
 const getPostData = (req) => {
   const promise = new Promise((resolve, reject) => {
     if (req.method !== 'POST') {
@@ -30,13 +32,45 @@ const getPostData = (req) => {
   return promise;
 };
 
+const getCookieExpires = () => {
+  const d = new Date();
+  d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
+  return d.toGMTString();
+};
+
 const serverHandle = (req, res) => {
   res.setHeader('Content-type', 'application/json');
 
   const url = req.url;
   req.path = url.split('?')[0];
 
+  //attach query
   req.query = queryString.parse(url.split('?')[1]);
+
+  //attach cookie
+  req.cookie = {};
+  const cookieStr = req.headers.cookie || '';
+  cookieStr.split(';').forEach((item) => {
+    if (!item) {
+      return;
+    }
+    const [key, val] = item.split('=');
+    req.cookie[key.trim()] = val.trim();
+  });
+
+  // attach session
+  let needSetCookie = false;
+  let userId = req.cookie.userid;
+  if (userId) {
+    if (!SESSION_DATA[userId]) {
+      SESSION_DATA[userId] = {};
+    }
+  } else {
+    needSetCookie = true;
+    userId = `${Date.now()}_${Math.random()}`;
+    SESSION_DATA[userId] = {};
+  }
+  req.session = SESSION_DATA[userId];
 
   getPostData(req).then((postData) => {
     req.body = postData;
@@ -44,6 +78,12 @@ const serverHandle = (req, res) => {
     const blogResult = handleBlogRouter(req, res);
     if (blogResult) {
       blogResult.then((blogData) => {
+        if (needSetCookie) {
+          res.setHeader(
+            'Set-Cookie',
+            `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`
+          );
+        }
         return res.end(JSON.stringify(blogData));
       });
 
@@ -53,6 +93,12 @@ const serverHandle = (req, res) => {
     const userResult = handleUserRouter(req, res);
     if (userResult) {
       userResult.then((userData) => {
+        if (needSetCookie) {
+          res.setHeader(
+            'Set-Cookie',
+            `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`
+          );
+        }
         return res.end(JSON.stringify(userData));
       });
 
--- src/router/user.js
+++ src/router/user.js
@@ -1,18 +1,32 @@
-const { loginCheck } = require('../controller/user');
+const { login } = require('../controller/user');
 const { SuccessModel, ErrorModel } = require('../model/resModel');
 
 const handleUserRouter = (req, res) => {
   const method = req.method;
 
-  if (method === 'POST' && req.path === '/api/user/login') {
-    const result = loginCheck(req.body);
+  if (method === 'GET' && req.path === '/api/user/login') {
+    const { username, password } = req.query;
+    const result = login(username, password);
 
     return result.then((data) => {
       if (data.username) {
+        req.session.username = data.username;
+        req.session.realname = data.realname;
         return new SuccessModel();
       }
       return new ErrorModel('Error when login');
     });
+  }
+
+  if (method === 'GET' && req.path === '/api/user/login-test') {
+    if (req.session.username) {
+      return Promise.resolve(
+        new SuccessModel({
+          session: req.session,
+        })
+      );
+    }
+    return Promise.resolve(new ErrorModel('Not login'));
   }
 };
 
